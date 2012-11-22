b4de4739abc61126ce2f95a09363039fdb6bf100
Paul Barrick <paul@imperativedesign.net>
2012-11-22 13:39:01 -0500

    initial commit

---
+++ setup/normal/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<!--[if lt IE 7]>      <html class="ie6"> <![endif]-->
+<!--[if IE 7]>         <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if gt IE 8]><!--> <html>         <!--<![endif]-->
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<title>Page Title</title>
+	<!-- 
+	<link rel="stylesheet" href="styles.css" type="text/css" media="screen" title="no title" charset="utf-8">
+	-->
+	
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/data.js"></script>
+	<script type="text/javascript" src="js/underscore.js"></script>
+	<script type="text/javascript" src="js/backbone.js"></script>
+	<script type="text/javascript" src="js/global.js" charset="utf-8"></script>
+</head>
+<body>
+	<div id="content"></div><!-- #content -->
+</body>
+</html>
---
+++ setup/normal/js/data.js
@@ -0,0 +1,22 @@
+var roledex = {
+	people: {
+		'Paul': {
+			first_name: 'Paul',
+			last_name: 'Barrick',
+			occupation: 'Developer',
+			phone: '678-617-5386'
+		},
+		'Alina': {
+			first_name: 'Alina',
+			last_name: 'Barrick',
+			occupation: 'Student',
+			phone: '770-377-7822'
+		},
+		'James': {
+			first_name: 'James',
+			last_name: 'Lakey',
+			occupation: 'Developer',
+			phone: '770-867-5309'
+		}
+	} //closes people
+} //closes roledex
